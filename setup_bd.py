4f5bed5d3d95a279d501ca8980d105ef759ee537
Tarun Karuturi <tkarutur@andrew.cmu.edu>
2016-06-15 23:29:48 -0700

    Bug in initializing mongodb script

--- setup_bd.py
+++ setup_bd.py
@@ -6,10 +6,13 @@ from werkzeug.security import generate_password_hash
 client = MongoClient()
 db = client.buildingdepot
 db.user.insert({"email":"admin@buildingdepot.org",
-		"password":generate_password_hash("admin"),
-		"first_name":"Admin",
-		"first_login":True,
-		"role":"super"})
+                "password":generate_password_hash("admin"),
+                "first_name":"Admin",
+                "first_login":True,
+                "role":"super"})
 db.data_service.insert({'name':'ds1',
-			'host':'127.0.0.1',
-			'port':82})
+                        'description':'',
+                        'host':'127.0.0.1',
+                        'port':82,
+                        'buildings':[],
+                        'admins':[]})
