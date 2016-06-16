ebcd119350a234a90e15c046d5c539e8a2fe9de3
Tarun Karuturi <tkarutur@andrew.cmu.edu>
2016-06-15 22:59:21 -0700

    Modified installation script to create default user and dataservice on installation

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ DEPLOY_DS=true
 # Check and make sure we are running as root or sudo (?)
 ################################################################################
 if [[ $UID -ne 0 ]]; then
-	echo -e "\n$0 must be run as root. Most functions require super-user priviledges!\n"
-	exit 1
+    echo -e "\n$0 must be run as root. Most functions require super-user priviledges!\n"
+    exit 1
 fi
 
 cp configs/nginx.conf /etc/nginx/nginx.conf
@@ -21,134 +21,138 @@ mkdir -p /var/sockets
 
 # Deploy apps
 function deploy_centralservice {
-	setup_venv /srv/buildingdepot/
-    
-	#copy and untar new dataservice tarball
-	cp -r buildingdepot/CentralService /srv/buildingdepot/
-	cp -r buildingdepot/DataService /srv/buildingdepot/
-	cp -r buildingdepot/CentralReplica /srv/buildingdepot/
-	cp -r buildingdepot/OAuth2Server /srv/buildingdepot/
-	cp -r buildingdepot/Documentation /srv/buildingdepot/
-	cd /srv/buildingdepot
-	# copy uwsgi files
-	cp configs/uwsgi_cs.ini /etc/uwsgi/apps-available/cs.ini
+    setup_venv /srv/buildingdepot/
 
-	# Create supervisor config
-	cp configs/supervisor-cs.conf /etc/supervisor/conf.d/
+    #copy and untar new dataservice tarball
+    cp -r buildingdepot/CentralService /srv/buildingdepot/
+    cp -r buildingdepot/DataService /srv/buildingdepot/
+    cp -r buildingdepot/CentralReplica /srv/buildingdepot/
+    cp -r buildingdepot/OAuth2Server /srv/buildingdepot/
+    cp -r buildingdepot/Documentation /srv/buildingdepot/
+    cd /srv/buildingdepot
+    # copy uwsgi files
+    cp configs/uwsgi_cs.ini /etc/uwsgi/apps-available/cs.ini
 
-	# Create supervisor config for central replica
-	cp configs/supervisor-replica.conf /etc/supervisor/conf.d/
+    # Create supervisor config
+    cp configs/supervisor-cs.conf /etc/supervisor/conf.d/
 
-	# Create nginx config
-	rm -f /etc/nginx/sites-enabled/default
+    # Create supervisor config for central replica
+    cp configs/supervisor-replica.conf /etc/supervisor/conf.d/
+
+    # Create nginx config
+    rm -f /etc/nginx/sites-enabled/default
 }
 
 function deploy_dataservice {
-	setup_venv /srv/buildingdepot/
-	
-	cd /srv/buildingdepot
+    setup_venv /srv/buildingdepot/
 
-	# copy uwsgi files
-	cp configs/uwsgi_ds.ini /etc/uwsgi/apps-available/ds.ini
+    cd /srv/buildingdepot
 
-	# Create supervisor config
-	cp configs/supervisor-ds.conf /etc/supervisor/conf.d/
+    # copy uwsgi files
+    cp configs/uwsgi_ds.ini /etc/uwsgi/apps-available/ds.ini
 
-	# Create nginx config
-	rm -f /etc/nginx/sites-enabled/default
+    # Create supervisor config
+    cp configs/supervisor-ds.conf /etc/supervisor/conf.d/
+
+    # Create nginx config
+    rm -f /etc/nginx/sites-enabled/default
 }
 
 
 function joint_deployment_fix {
-	# Create join nginx config
-	rm -f /etc/nginx/sites-enabled/default
-	cd /srv/buildingdepot
-	#Setting up SSL
-	echo "Do you have a SSL certificate and key that you would like to use? Please enter [y/n]"
-	read response
-	#If user already has certificate
-	if [ "$response" == "y" ]; then
-    		echo "Please enter the path to the certificate:"
-    		read cert_path
-    		sed -i "s|<cert_path>|$cert_path|g" /srv/buildingdepot/configs/together_ssl.conf
-    		echo "Please enter the path to the key:"
-    		read key_path
-    		sed -i "s|<key_path>|$key_path|g" /srv/buildingdepot/configs/together_ssl.conf
-		echo "Please enter the ip address or the domain name of this installation"
-        	read domain
-        	sed -i "s|<domain>|$domain|g" /srv/buildingdepot/configs/together_ssl.conf
-		cp configs/together_ssl.conf /etc/nginx/sites-available/together.conf
-	else 
-		cp configs/together.conf /etc/nginx/sites-available/together.conf
-	fi
-	ln -sf /etc/nginx/sites-available/together.conf /etc/nginx/sites-enabled/together.conf
+    # Create join nginx config
+    rm -f /etc/nginx/sites-enabled/default
+    cd /srv/buildingdepot
+    #Setting up SSL
+    echo "Do you have a SSL certificate and key that you would like to use? Please enter [y/n]"
+    read response
+    #If user already has certificate
+    if [ "$response" == "y" ]; then
+            echo "Please enter the path to the certificate:"
+            read cert_path
+            sed -i "s|<cert_path>|$cert_path|g" /srv/buildingdepot/configs/together_ssl.conf
+            echo "Please enter the path to the key:"
+            read key_path
+            sed -i "s|<key_path>|$key_path|g" /srv/buildingdepot/configs/together_ssl.conf
+        echo "Please enter the ip address or the domain name of this installation"
+            read domain
+            sed -i "s|<domain>|$domain|g" /srv/buildingdepot/configs/together_ssl.conf
+        cp configs/together_ssl.conf /etc/nginx/sites-available/together.conf
+    else
+        cp configs/together.conf /etc/nginx/sites-available/together.conf
+    fi
+    ln -sf /etc/nginx/sites-available/together.conf /etc/nginx/sites-enabled/together.conf
 }
 
 function deploy_config {
-	cp -r configs/ /srv/buildingdepot
-	mkdir /var/sockets
+    cp -r configs/ /srv/buildingdepot
+    mkdir /var/sockets
 }
 
 function install_packages {
-	echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-	apt-get update
-	apt-get install
-	apt-get -y install python-pip
-	apt-get install -y mongodb
-	apt-get install -y openssl python-setuptools python-dev build-essential python-software-properties
-	apt-get install -y nginx
-	apt-get install -y supervisor
-	apt-get install -y redis-server
-	pip install --upgrade virtualenv
-	apt-get install wget
-	wget http://influxdb.s3.amazonaws.com/influxdb_0.9.2_amd64.deb
-	sudo dpkg -i influxdb_0.9.2_amd64.deb
- 	sudo apt-get install rabbitmq-server
+    echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+    apt-get update
+    apt-get install
+    apt-get -y install python-pip
+    apt-get install -y mongodb
+    apt-get install -y openssl python-setuptools python-dev build-essential python-software-properties
+    apt-get install -y nginx
+    apt-get install -y supervisor
+    apt-get install -y redis-server
+    pip install --upgrade virtualenv
+    apt-get install wget
+    wget http://influxdb.s3.amazonaws.com/influxdb_0.9.2_amd64.deb
+    sudo dpkg -i influxdb_0.9.2_amd64.deb
+    sudo apt-get install rabbitmq-server
+    sudo apt-get install mailutils
+    sed -i -e 's/"inet_interfaces = all/"inet_interfaces = loopback-only"/g' /etc/postfix/main.cf
+    service postfix restart
 }
- 
+
 function setup_venv {
-	cp pip_packages.list $1
-	cd $1
+    cp pip_packages.list $1
+    cd $1
 
-	virtualenv ./venv
-	source venv/bin/activate
+    virtualenv ./venv
+    source venv/bin/activate
 
-	pip install --upgrade pip
-	pip install --upgrade setuptools
-	pip install --upgrade -r pip_packages.list
+    pip install --upgrade pip
+    pip install --upgrade setuptools
+    pip install --upgrade -r pip_packages.list
 
-	pip install --upgrade uWSGI
-	mkdir -p /etc/uwsgi/apps-available/
+    pip install --upgrade uWSGI
+    mkdir -p /etc/uwsgi/apps-available/
 
-	deactivate
-	cd -
+    deactivate
+    cd -
 }
 
 deploy_config
 install_packages
 if [ "$DEPLOY_CS" = true ]; then
-	deploy_centralservice
+    deploy_centralservice
 fi
 
 if [ "$DEPLOY_DS" = true ]; then
-	deploy_dataservice
+    deploy_dataservice
 fi
 
 /etc/init.d/mongodb start
 service influxdb start
 service supervisor stop
-service supervisor start 
+service supervisor start
 sleep 5
 supervisorctl restart all
 service influxdb start
 
 if [ "$DEPLOY_TOGETHER" = true ]; then
-	joint_deployment_fix
-	service nginx restart
+    joint_deployment_fix
+    service nginx restart
 fi
 
 rm -rf configs
 
 curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE buildingdepot"
 echo -e "\nInstallation Finished..\n"
-
+/srv/buildingdepot/venv/bin/python2.7 setup_bd.py
+echo -e "Created a super user with following credentials. Please login and change password immediately \n user id : admin@buildingdepot.org \n password: admin"
---
+++ setup_bd.py
@@ -0,0 +1,15 @@
+#!/srv/buildingdepot/venv/bin/python2.7
+
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+
+client = MongoClient()
+db = client.buildingdepot
+db.user.insert({"email":"admin@buildingdepot.org",
+		"password":generate_password_hash("admin"),
+		"first_name":"Admin",
+		"first_login":True,
+		"role":"super"})
+db.data_service.insert({'name':'ds1',
+			'host':'127.0.0.1',
+			'port':82})
