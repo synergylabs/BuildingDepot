fc27c07ac6638315553b0db68d77924ac3cec6a4
sudershan <sud335@gmail.com>
2016-08-02 11:58:20 +0530

    Central Service Changes:  -Remove subscribers and write completely(we can add later if needed)  -After a new user is successfully added, the UI will now refresh automatically.  -Resolved Major Gateway errors.  -minor ui changes

--- buildingdepot/CentralService/app/rpc/defs.py
+++ buildingdepot/CentralService/app/rpc/defs.py
@@ -1,15 +1,17 @@
 from .. import svr
-from ..rest_api.helper import get_ds,get_sg_ds
+from ..rest_api.helper import get_ds, get_sg_ds
 from ..models.cs_models import DataService
 from xmlrpclib import ServerProxy
 
-def create_sensor(sensor_id,email,building):
-    svr = get_remote(get_ds(sensor_id,building))
+
+def create_sensor(sensor_id, email, building):
+    svr = get_remote(get_ds(sensor_id, building))
     try:
-        svr.create_sensor(sensor_id,email)
+        svr.create_sensor(sensor_id, email)
     except Exception as e:
         return False
     return True
+
 
 def invalidate_sensor(sensor_id):
     svr = get_remote(get_ds(sensor_id))
@@ -19,6 +21,7 @@ def invalidate_sensor(sensor_id):
         return False
     return True
 
+
 def delete_sensor(sensor_id):
     svr = get_remote(get_ds(sensor_id))
     try:
@@ -27,21 +30,24 @@ def delete_sensor(sensor_id):
         return False
     return True
 
-def create_permission(user_group,sensor_group,email,permission):
+
+def create_permission(user_group, sensor_group, email, permission):
     svr = get_remote(get_sg_ds(sensor_group))
     try:
-        svr.create_permission(user_group,sensor_group,email,permission)
+        svr.create_permission(user_group, sensor_group, email, permission)
     except Exception as e:
         return False
     return True
 
-def delete_permission(user_group,sensor_group):
+
+def delete_permission(user_group, sensor_group):
     svr = get_remote(get_sg_ds(sensor_group))
     try:
-        svr.delete_permission(user_group,sensor_group)
+        svr.delete_permission(user_group, sensor_group)
     except Exception as e:
         return False
     return True
+
 
 def invalidate_permission(sensor_group):
     svr = get_remote(get_sg_ds(sensor_group))
@@ -51,14 +57,16 @@ def invalidate_permission(sensor_group):
         return False
     return True
 
-def invalidate_user(user_group,email):
+
+def invalidate_user(user_group, email):
     svr = get_remote(get_sg_ds(sensor_group))
     try:
-        svr.invalidate_user(usergroup,email)
+        svr.invalidate_user(usergroup, email)
     except Exception as e:
         return False
     return True
 
+
 def get_remote(dataservice):
     ds = DataService.objects(name=dataservice).first()
-    return ServerProxy("http://"+ds.host+":"+"8080")
+    return ServerProxy("http://" + ds.host + ":" + "8080")
