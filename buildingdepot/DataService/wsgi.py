5ae62c475f6b618ba98c2e8a2c9b94456a06de24
Jason Koh <bk7749@gmail.com>
2019-01-02 18:11:39 -0800

    update package name along with Flask update

--- buildingdepot/DataService/wsgi.py
+++ buildingdepot/DataService/wsgi.py
@@ -14,7 +14,7 @@ config file or falls back to the default one.
 
 import os
 from app import create_app
-from flask.ext.script import Manager, Shell, Server
+from flask_script import Manager, Shell, Server
 from app.rest_api.register import register_view
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'dev')
