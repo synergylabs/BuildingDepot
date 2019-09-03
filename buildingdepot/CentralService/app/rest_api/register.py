ab185e04368acee4a8ebda7f1fa5307ce9202035
Shreyas Nagare <shreyasnagare@gmail.com>
2019-09-03 15:18:28 -0400

    Register sensor view APIs

--- buildingdepot/CentralService/app/rest_api/register.py
+++ buildingdepot/CentralService/app/rest_api/register.py
@@ -5,7 +5,7 @@ from .dataservices import dataservice, ds_buildings, ds_admins
 from .users import user
 from .sensorgroups import sensorgroup, sg_tags
 from .usergroups import ug_users, usergroup
-from .sensors import sensor, search, sensor_tags
+from .sensors import sensor, search, sensor_tags, sensor_views
 from .permissions import permission
 
 
@@ -60,6 +60,14 @@ def register_view(app_obj):
     app_obj.add_url_rule('/api/sensor/<name>', view_func=sensor_view, methods=['GET', 'DELETE'])
     # create a new sensor
     app_obj.add_url_rule('/api/sensor', view_func=sensor_view, methods=['POST'])
+
+    sensor_view_view = sensor_views.SensorViewService.as_view('sensor_view_api')
+    # get views of sensor by name
+    app_obj.add_url_rule('/api/sensor/<name>/views', view_func=sensor_view_view, methods=['GET'])
+    # delete views of sensor by name
+    app_obj.add_url_rule('/api/sensor/<name>/views/<uuid>', view_func=sensor_view_view, methods=['DELETE'])
+    # create a new views for sensor
+    app_obj.add_url_rule('/api/sensor/<name>/views', view_func=sensor_view_view, methods=['POST'])
 
     sensortags_view = sensor_tags.SensorTagsService.as_view('sensortags_api')
     # get gets a sensor's tags
