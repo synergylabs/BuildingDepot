6b7bc419590571b3cc51d9d59d0b90dab727ada7
Shreyas Nagare <shreyasnagare@gmail.com>
2020-01-23 21:04:10 -0500

    Views can;t create arbitrary tag value pairs, update comments

--- buildingdepot/CentralService/app/rest_api/sensors/sensor_views.py
+++ buildingdepot/CentralService/app/rest_api/sensors/sensor_views.py
@@ -14,7 +14,7 @@ ensure that the cache gets updated as needed.
 from flask import request, jsonify
 from flask.views import MethodView
 from .. import responses
-from ...models.cs_models import Sensor
+from ...models.cs_models import Sensor, Building
 from uuid import uuid4
 from ... import r, oauth
 from ..helper import get_email, xstr, get_building_choices, check_oauth
@@ -25,7 +25,7 @@ class SensorViewService(MethodView):
     @check_oauth
     def get(self, name):
         """
-        Retrieve sensor details based on uuid specified
+        Retrieve sensor view details based on sensor uuid specified
 
         Args as data:
         name : <name of sensor>
@@ -66,7 +66,7 @@ class SensorViewService(MethodView):
     @check_oauth
     def post(self, name):
         """
-        Creates sensor if the building specified is valid
+        Creates sensor view if the building and sensor specified are valid
 
         Args as data:
         "source_name":<name-of-view>
@@ -101,30 +101,38 @@ class SensorViewService(MethodView):
             tags = []
         fields_list = fields.split(',')
         field_tags = [{"name": "field", "value": field.strip()} for field in fields_list]
+        available_tags = Building.objects(name=building).first().tags
+        available_tags = [{"name": tag.name, "value": tag.value} for tag in available_tags]
         tags = tags + [{"name": tag.name, "value": tag.value} for tag in sensor.tags] + [{"name": "parent", "value": sensor.name}] + field_tags
-        if building in get_building_choices("rest_api"):
-            uuid = str(uuid4())
-            if defs.create_sensor(uuid, email, building, fields, name):
-                Sensor(name=uuid,
-                       source_name=xstr(view_name),
-                       source_identifier="SensorView",
-                       building=building,
-                       owner=email,
-                       tags=tags).save()
-                r.set('owner:{}'.format(uuid), email)
-                r.sadd('views:{}'.format(name), uuid)
-                r.set('fields:{}'.format(uuid), fields)
-                r.set('parent:{}'.format(uuid), name)
-                fields = [field.strip() for field in fields.split(",")]
-                for field in fields:
-                    r.sadd('{}:{}'.format(name, field), uuid)
-                r.sadd('views', uuid)
-                response = dict(responses.success_true)
-                response.update({'id': uuid})
-                return jsonify(response)
-            else:
-                return jsonify(responses.ds_error)
-        return jsonify(responses.invalid_building)
+        print (available_tags)
+        print (tags)
+        if any(tag not in available_tags for tag in tags):
+            missing_tags = set([(tag['name'], tag['value']) for tag in tags]) - set([(tag['name'], tag['value']) for tag in available_tags])
+            missing_tags = [{'name': name, 'value': value} for name, value in missing_tags]
+            return jsonify({'success': 'False', 'building_tags_required': missing_tags, 'error': 'Operation requires '
+                                                                                                 'additional building '
+                                                                                                 'tags.'})
+        uuid = str(uuid4())
+        if defs.create_sensor(uuid, email, building, fields, name):
+            Sensor(name=uuid,
+                   source_name=xstr(view_name),
+                   source_identifier="SensorView",
+                   building=building,
+                   owner=email,
+                   tags=tags).save()
+            r.set('owner:{}'.format(uuid), email)
+            r.sadd('views:{}'.format(name), uuid)
+            r.set('fields:{}'.format(uuid), fields)
+            r.set('parent:{}'.format(uuid), name)
+            fields = [field.strip() for field in fields.split(",")]
+            for field in fields:
+                r.sadd('{}:{}'.format(name, field), uuid)
+            r.sadd('views', uuid)
+            response = dict(responses.success_true)
+            response.update({'id': uuid})
+            return jsonify(response)
+        else:
+            return jsonify(responses.ds_error)
 
     @check_oauth
     def delete(self, name, uuid):
