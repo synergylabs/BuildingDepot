6c2d4a72b6f0bc2ed8210472bfff4e131ed4f793
shreyasnagare <shreyasnagare@gmail.com>
2020-12-22 20:09:24 +0530

    Update to Python3
    - Update GH actions
    - Update copyright header

--- buildingdepot/CentralService/app/rest_api/notifications/notification.py
+++ buildingdepot/CentralService/app/rest_api/notifications/notification.py
@@ -4,8 +4,8 @@ DataService.rest_api.notifcation
 
 This module handles setting up and managing notifications for a given ID.
 
-@copyright: (c) 2016 SynergyLabs
-@license: UCSD License. See License file for details.
+@copyright: (c) 2020 SynergyLabs
+@license: CMU License. See License file for details.
 """
 
 from flask import request, jsonify, Flask
@@ -15,6 +15,7 @@ from .. import responses
 from ..helper import check_oauth, get_email
 from ...models.cs_models import NotificationClientId
 
+
 def get_notification_client_id(client_email):
     notification_client = NotificationClientId.objects(email=client_email).first()
 
@@ -23,18 +24,19 @@ def get_notification_client_id(client_email):
 
     return None
 
+
 class NotificationClientIdService(MethodView):
     @check_oauth
     def post(self):
-        data = request.get_json()['data']
-        print 'We got the request'
-        print 'Want to save the ID ' + data['id']
-        if data is None or data['id'] is None:
+        data = request.get_json()["data"]
+        print("We got the request")
+        print("Want to save the ID " + data["id"])
+        if data is None or data["id"] is None:
             return jsonify(responses.missing_parameters)
-        print 'No params missing'
+        print("No params missing")
         if get_notification_client_id(get_email()) is None:
-            print 'Saving the ID for client ' + str(get_email())
-            NotificationClientId(email=get_email(), client_id=data['id']).save()
+            print("Saving the ID for client " + str(get_email()))
+            NotificationClientId(email=get_email(), client_id=data["id"]).save()
         else:
             return jsonify(responses.client_id_already_exists)
 
@@ -42,13 +44,13 @@ class NotificationClientIdService(MethodView):
 
     @check_oauth
     def put(self):
-        data = request.get_json()['data']
+        data = request.get_json()["data"]
 
-        if data is None or data['id'] is None:
+        if data is None or data["id"] is None:
             return jsonify(responses.missing_parameters)
 
         id = NotificationClientId(email=get_email()).first()
-        id.client_id = data['id']
+        id.client_id = data["id"]
         id.update()
 
         return jsonify(responses.success_true)
