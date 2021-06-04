0204d63b1d68483c519325302196cea3991c1f1f
Sudershan Boovaraghavan <sud335@gmail.com>
2021-06-04 04:50:19 -0400

    Update app subscription API to handle batch inputs

--- buildingdepot/DataService/app/rest_api/app_subscription.py
+++ buildingdepot/DataService/app/rest_api/app_subscription.py
@@ -11,46 +11,55 @@ It also accordingly handles the unsubscription of sensors from the apps.
 """
 
 from flask.views import MethodView
-from flask import request,jsonify
+from flask import request, jsonify
 from ..models.ds_models import Application
-from .. import r,oauth,exchange
-import sys,traceback
-from .helper import connect_broker,get_email, check_oauth
+from .. import r, oauth, exchange
+import sys, traceback
+from .helper import connect_broker, get_email, check_oauth
+from . import responses
+
 
 class AppSubscriptionService(MethodView):
 
-    methods = ['POST','DELETE']
-
     @check_oauth
-    def dispatch_request(self):
+    def post(self):
+        """
+        Create Queue binding of sensor and user's applications with the given name.
+
+        Args as data:
+            "app" : <name of app>
+            "sensorID": <name of the sensor>
+        Returns (JSON):
+        {
+            "success": <True or False>
+            "error": <If success if False then reason for failure>
+        }
+        """
+
         json_data = request.get_json()['data']
         email = get_email()
         try:
             app_id = json_data['app']
             sensor = json_data['sensor']
         except Exception as e:
-            return jsonify({'success': 'False', 'error': 'Missing parameters'})
+            return jsonify(responses.missing_parameters)
 
         app_list = Application._get_collection().find({'user': email})[0]['apps']
-
         pubsub = connect_broker()
         if pubsub is None:
-            return jsonify({'success': 'False', 'error': 'Failed to connect to broker'})
+            return jsonify(responses.broker_connection_failure)
 
         for app in app_list:
             if app_id == app['value']:
                 try:
                     channel = pubsub.channel()
-                    if request.method == 'POST':
-                        channel.queue_bind(exchange=exchange, queue=app['value'], routing_key=sensor)
-                        r.sadd(''.join(['apps:', sensor]), app['value'])
-                    elif request.method == 'DELETE':
-                        channel.queue_unbind(exchange=exchange, queue=app['value'], routing_key=sensor)
-                        r.srem(''.join(['apps:', sensor]), app['value'])
+                    channel.queue_bind(exchange=exchange, queue=app['value'], routing_key=sensor)
+                    r.sadd(''.join(['apps:', sensor]), app['value'])
+
                 except Exception as e:
                     print "Failed to bind queue " + str(e)
                     print traceback.print_exc()
-                    return jsonify({'success': 'False', 'error': 'Failed to bind queue'})
+                    return jsonify(responses.queue_binding_failure)
 
                 if pubsub:
                     try:
@@ -59,7 +68,98 @@ class AppSubscriptionService(MethodView):
                     except Exception as e:
                         print "Failed to end RabbitMQ session" + str(e)
 
-                return jsonify({'success': 'True'})
+            return jsonify(responses.success_true)
 
-        return jsonify({'success': 'False', 'error': 'App id doesn\'t exist'})
+        return jsonify(responses.application_does_not_exist)
 
+    @check_oauth
+    def delete(self):
+        """
+        Delete Queue binding of sensor and user's applications with the given name.
+
+        Args as data:
+            "app" : <name of app>
+            "sensorID": <name of the sensor>
+        Returns (JSON):
+        {
+            "success": <True or False>
+            "error": <If success if False then reason for failure>
+        }
+        """
+
+        json_data = request.get_json()['data']
+        email = get_email()
+        try:
+            app_id = json_data['app']
+            sensor = json_data['sensor']
+        except Exception as e:
+            return jsonify(responses.missing_parameters)
+
+        app_list = Application._get_collection().find({'user': email})[0]['apps']
+        pubsub = connect_broker()
+        if pubsub is None:
+            return jsonify(responses.broker_connection_failure)
+
+        for app in app_list:
+            if app_id == app['value']:
+                try:
+                    channel = pubsub.channel()
+                    channel.queue_unbind(exchange=exchange, queue=app['value'], routing_key=sensor)
+                    r.srem(''.join(['apps:', sensor]), app['value'])
+                except Exception as e:
+                    print "Failed to bind queue " + str(e)
+                    print traceback.print_exc()
+                    return jsonify(responses.queue_binding_failure)
+
+                if pubsub:
+                    try:
+                        channel.close()
+                        pubsub.close()
+                    except Exception as e:
+                        print "Failed to end RabbitMQ session" + str(e)
+
+            return jsonify(responses.success_true)
+
+        return jsonify(responses.application_does_not_exist)
+
+    # @check_oauth
+    # def dispatch_request(self):
+    #     json_data = request.get_json()['data']
+    #     email = get_email()
+    #     try:
+    #         app_id = json_data['app']
+    #         sensor = json_data['sensor']
+    #     except Exception as e:
+    #         return jsonify({'success': 'False', 'error': 'Missing parameters'})
+    #
+    #     app_list = Application._get_collection().find({'user': email})[0]['apps']
+    #
+    #     pubsub = connect_broker()
+    #     if pubsub is None:
+    #         return jsonify({'success': 'False', 'error': 'Failed to connect to broker'})
+    #
+    #     for app in app_list:
+    #         if app_id == app['value']:
+    #             try:
+    #                 channel = pubsub.channel()
+    #                 if request.method == 'POST':
+    #                     channel.queue_bind(exchange=exchange, queue=app['value'], routing_key=sensor)
+    #                     r.sadd(''.join(['apps:', sensor]), app['value'])
+    #                 elif request.method == 'DELETE':
+    #                     channel.queue_unbind(exchange=exchange, queue=app['value'], routing_key=sensor)
+    #                     r.srem(''.join(['apps:', sensor]), app['value'])
+    #             except Exception as e:
+    #                 print "Failed to bind queue " + str(e)
+    #                 print traceback.print_exc()
+    #                 return jsonify({'success': 'False', 'error': 'Failed to bind queue'})
+    #
+    #             if pubsub:
+    #                 try:
+    #                     channel.close()
+    #                     pubsub.close()
+    #                 except Exception as e:
+    #                     print "Failed to end RabbitMQ session" + str(e)
+    #
+    #             return jsonify({'success': 'True'})
+    #
+    #     return jsonify({'success': 'False', 'error': 'App id doesn\'t exist'})
