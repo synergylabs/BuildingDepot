2bbe28cf33fc68da18e66775dabc8868ded3430d
Sudershan <sud335@gmail.com>
2021-07-15 21:04:42 -0400

    Reformat Code

--- install.sh
+++ install.sh
@@ -113,61 +113,61 @@ function install_packages() {
   apt-get install -y apt-transport-https
   source /etc/lsb-release
 
-    # Add keys for Rabbitmq
-    curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | bash
-    # Adds Launchpad PPA that provides modern Erlang releases
-    sudo apt-key adv --keyserver "keyserver.ubuntu.com" --recv-keys "F77F1EDA57EBB1CC"
-    echo "deb http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/rabbitmq-erlang.list
-    echo "deb-src http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu ${DISTRIB_CODENAME} main" | tee -a /etc/apt/sources.list.d/rabbitmq-erlang.list
+  # Add keys for Rabbitmq
+  curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | bash
+  # Adds Launchpad PPA that provides modern Erlang releases
+  sudo apt-key adv --keyserver "keyserver.ubuntu.com" --recv-keys "F77F1EDA57EBB1CC"
+  echo "deb http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/rabbitmq-erlang.list
+  echo "deb-src http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu ${DISTRIB_CODENAME} main" | tee -a /etc/apt/sources.list.d/rabbitmq-erlang.list
 
-    # Add keys to install influxdb
-    curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-    echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+  # Add keys to install influxdb
+  curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+  echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 
-    # Add keys to install mongodb
-    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-    if [ $DISTRIB_CODENAME == "focal" ]; then
-        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${DISTRIB_CODENAME}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-    elif [ $DISTRIB_CODENAME == "bionic" ]; then
-        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${DISTRIB_CODENAME}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-    elif [ $DISTRIB_CODENAME == "xenial" ]; then
-        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${DISTRIB_CODENAME}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-    elif [ $DISTRIB_CODENAME == "trusty" ]; then
-        wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
-        echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/${DISTRIB_ID,,} ${DISTRIB_CODENAME}/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-    fi
+  # Add keys to install mongodb
+  wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+  if [ $DISTRIB_CODENAME == "focal" ]; then
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${DISTRIB_CODENAME}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+  elif [ $DISTRIB_CODENAME == "bionic" ]; then
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${DISTRIB_CODENAME}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+  elif [ $DISTRIB_CODENAME == "xenial" ]; then
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${DISTRIB_CODENAME}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+  elif [ $DISTRIB_CODENAME == "trusty" ]; then
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
+    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/${DISTRIB_ID,,} ${DISTRIB_CODENAME}/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+  fi
 
-    apt-get update -y
-    apt-get install
-    apt-get -y install python3-pip
+  apt-get update -y
+  apt-get install
+  apt-get -y install python3-pip
 
-    if [ $DISTRIB_CODENAME == "trusty" ]; then
-      apt-get install -y mongodb-org=4.0.25 mongodb-org-server=4.0.25 mongodb-org-shell=4.0.25 mongodb-org-mongos=4.0.25 mongodb-org-tools=4.0.25
-    else
-      apt-get install -y mongodb-org=4.4.6 mongodb-org-server=4.4.6 mongodb-org-shell=4.4.6 mongodb-org-mongos=4.4.6 mongodb-org-tools=4.4.6
-    fi
+  if [ $DISTRIB_CODENAME == "trusty" ]; then
+    apt-get install -y mongodb-org=4.0.25 mongodb-org-server=4.0.25 mongodb-org-shell=4.0.25 mongodb-org-mongos=4.0.25 mongodb-org-tools=4.0.25
+  else
+    apt-get install -y mongodb-org=4.4.6 mongodb-org-server=4.4.6 mongodb-org-shell=4.4.6 mongodb-org-mongos=4.4.6 mongodb-org-tools=4.4.6
+  fi
 
-    apt-get install -y openssl python3-setuptools python3-dev build-essential software-properties-common
-    apt-get install -y nginx
-    apt-get install -y supervisor
-    apt-get install -y redis-server
-    pip3 install --upgrade virtualenv
-    apt-get install -y wget
-    apt-get install -y influxdb
-    service influxdb start
-    service mongod start
-    apt-get install -y erlang-base \
-                  erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
-                  erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
-                  erlang-runtime-tools erlang-snmp erlang-ssl \
-                  erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
-    apt-get install -y rabbitmq-server
-    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-    apt-get install -y nodejs
-    apt-get install -y npm
-    sed -i -e 's/"inet_interfaces = all/"inet_interfaces = loopback-only"/g' /etc/postfix/main.cf
-    service postfix restart
-    systemctl enable mongod.service
+  apt-get install -y openssl python3-setuptools python3-dev build-essential software-properties-common
+  apt-get install -y nginx
+  apt-get install -y supervisor
+  apt-get install -y redis-server
+  pip3 install --upgrade virtualenv
+  apt-get install -y wget
+  apt-get install -y influxdb
+  service influxdb start
+  service mongod start
+  apt-get install -y erlang-base \
+    erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+    erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+    erlang-runtime-tools erlang-snmp erlang-ssl \
+    erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+  apt-get install -y rabbitmq-server
+  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+  apt-get install -y nodejs
+  apt-get install -y npm
+  sed -i -e 's/"inet_interfaces = all/"inet_interfaces = loopback-only"/g' /etc/postfix/main.cf
+  service postfix restart
+  systemctl enable mongod.service
 }
 
 function setup_gmail() {
@@ -231,106 +231,106 @@ function setup_email() {
 }
 
 function setup_notifications() {
-    echo "BuildingDepot uses notifications to alert users or systems of events in real-time. By default, "
-    echo "BuildingDepot uses RabbitMQ to deliver messages and we also supports Google Firebase Cloud Messaging (FCM), "
-    echo "which allows BuildingDepot to send push notifications to mobile users."
-    echo "Enter Y to select Google FCM and N to select RabbitMQ: "
+  echo "BuildingDepot uses notifications to alert users or systems of events in real-time. By default, "
+  echo "BuildingDepot uses RabbitMQ to deliver messages and we also supports Google Firebase Cloud Messaging (FCM), "
+  echo "which allows BuildingDepot to send push notifications to mobile users."
+  echo "Enter Y to select Google FCM and N to select RabbitMQ: "
+  read response
+  if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
+    pip3 install "firebase-admin==2.18.0"
+    echo "Please provide the absolute path of where your Google Service Account JSON file is, which contains the keys for your FCM project."
     read response
-    if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
-        pip3 install "firebase-admin==2.18.0"
-        echo "Please provide the absolute path of where your Google Service Account JSON file is, which contains the keys for your FCM project."
-        read response
-        if [ ! -z "$response" ]; then
-            echo "NOTIFICATION_TYPE = 'FIREBASE'" >> $BD/CentralService/cs_config
-            echo "FIREBASE_CREDENTIALS = '$response'" >> $BD/CentralService/cs_config
-        fi
-    elif [ "$response" == "N" ] || [ "$response" == "n" ]; then
-        echo "NOTIFICATION_TYPE = 'RabbitMQ'" >> $BD/CentralService/cs_config
+    if [ ! -z "$response" ]; then
+      echo "NOTIFICATION_TYPE = 'FIREBASE'" >>$BD/CentralService/cs_config
+      echo "FIREBASE_CREDENTIALS = '$response'" >>$BD/CentralService/cs_config
     fi
+  elif [ "$response" == "N" ] || [ "$response" == "n" ]; then
+    echo "NOTIFICATION_TYPE = 'RabbitMQ'" >>$BD/CentralService/cs_config
+  fi
 }
 
 function setup_packages() {
 
-    echo
-    echo "Securing BD Packages"
-    echo "--------------------"
-    echo "Enter Y to auto-generate credentials for packages (MongoDB,InfluxDB & Redis). Credentials are stored in cs_config file (or) Enter N to input individual package credentials"
+  echo
+  echo "Securing BD Packages"
+  echo "--------------------"
+  echo "Enter Y to auto-generate credentials for packages (MongoDB,InfluxDB & Redis). Credentials are stored in cs_config file (or) Enter N to input individual package credentials"
+  read response
+  if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
+    ## Add MongoDB Admin user
+    mongoUsername="user$(openssl rand -hex 16)"
+    mongoPassword=$(openssl rand -hex 32)
+    echo "MONGODB_USERNAME = '$mongoUsername'" >>$BD/CentralService/cs_config
+    echo "MONGODB_PWD = '$mongoPassword'" >>$BD/CentralService/cs_config
+    echo "MONGODB_USERNAME = '$mongoUsername'" >>$BD/DataService/ds_config
+    echo "MONGODB_PWD = '$mongoPassword'" >>$BD/DataService/ds_config
+    echo "    MONGODB_USERNAME = '$mongoUsername'" >>$BD/CentralReplica/config.py
+    echo "    MONGODB_PWD = '$mongoPassword'" >>$BD/CentralReplica/config.py
+    mongo --eval "db.getSiblingDB('admin').createUser({user:'$mongoUsername',pwd:'$mongoPassword',roles:['userAdminAnyDatabase','dbAdminAnyDatabase','readWriteAnyDatabase']})"
+    # Enable MongoDB authorization
+    echo "security:" >>/etc/mongod.conf
+    echo "  authorization: \"enabled\"" >>/etc/mongod.conf
+    service mongod restart
+
+    sleep 2
+
+    ## Add InfluxDB Admin user
+    influxUsername="user$(openssl rand -hex 16)"
+    influxPassword=$(openssl rand -hex 32)
+    echo "INFLUXDB_USERNAME = '$influxUsername'" >>$BD/DataService/ds_config
+    echo "INFLUXDB_PWD = '$influxPassword'" >>$BD/DataService/ds_config
+    sleep 1
+    curl -d "q=CREATE USER $influxUsername WITH PASSWORD '$influxPassword' WITH ALL PRIVILEGES" -X POST http://localhost:8086/query
+    sed -ir 's/# auth-enabled = false/auth-enabled = true/g' /etc/influxdb/influxdb.conf
+    service influxdb restart
+
+    sleep 2
+
+    ## Add Redis Admin user
+    redisPassword=$(openssl rand -hex 64)
+    echo "REDIS_PWD = '$redisPassword'" >>$BD/CentralService/cs_config
+    echo "REDIS_PWD = '$redisPassword'" >>$BD/DataService/ds_config
+    echo "    REDIS_PWD = '$redisPassword'" >>$BD/CentralReplica/config.py
+    sed -i -e '/#.* requirepass / s/.*/ requirepass  '$redisPassword'/' /etc/redis/redis.conf
+    service redis restart
+
+    sleep 2
+
+    ## Add RabbitMQ Admin user
+    rabbitmqUsername="user$(openssl rand -hex 16)"
+    rabbitmqPassword=$(openssl rand -hex 32)
+    rabbitmqUsername_endUser="user$(openssl rand -hex 16)"
+    rabbitmqPassword_endUser=$(openssl rand -hex 32)
+    echo "RABBITMQ_ADMIN_USERNAME = '$rabbitmqUsername'" >>$BD/DataService/ds_config
+    echo "RABBITMQ_ADMIN_PWD = '$rabbitmqPassword'" >>$BD/DataService/ds_config
+    echo "RABBITMQ_ENDUSER_USERNAME = '$rabbitmqUsername_endUser'" >>$BD/DataService/ds_config
+    echo "RABBITMQ_ENDUSER_PWD = '$rabbitmqPassword_endUser'" >>$BD/DataService/ds_config
+    # Create a Admin user.
+    rabbitmqctl add_user "$rabbitmqUsername" "$rabbitmqPassword"
+    # Add Administrative Rights
+    rabbitmqctl set_user_tags "$rabbitmqUsername" administrator
+    # Grant necessary permissions
+    rabbitmqctl set_permissions -p / "$rabbitmqUsername" ".*" ".*" ".*"
+    # Create a End User.
+    rabbitmqctl add_user "$rabbitmqUsername_endUser" "$rabbitmqPassword_endUser"
+    # Add Permissions
+    rabbitmqctl set_user_tags "$rabbitmqUsername_endUser"
+    # Grant necessary permissions
+    rabbitmqctl set_permissions -p / "$rabbitmqUsername_endUser" "" "" ".*"
+
+    echo "BuildingDepot uses RabbitMQ Queues for Publishing and Subscribing to Sensor data. "
+    echo "Some web front-end use RabbitMQ Queues use rabbitmq_web_stomp plugin"
+    echo "Enter Y to install rabbitmq_web_stomp plugin: "
     read response
     if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
-        ## Add MongoDB Admin user
-        mongoUsername="user$(openssl rand -hex 16)"
-        mongoPassword=$(openssl rand -hex 32)
-        echo "MONGODB_USERNAME = '$mongoUsername'" >> $BD/CentralService/cs_config
-        echo "MONGODB_PWD = '$mongoPassword'" >> $BD/CentralService/cs_config
-        echo "MONGODB_USERNAME = '$mongoUsername'" >> $BD/DataService/ds_config
-        echo "MONGODB_PWD = '$mongoPassword'" >> $BD/DataService/ds_config
-        echo "    MONGODB_USERNAME = '$mongoUsername'" >> $BD/CentralReplica/config.py
-        echo "    MONGODB_PWD = '$mongoPassword'" >> $BD/CentralReplica/config.py
-        mongo --eval "db.getSiblingDB('admin').createUser({user:'$mongoUsername',pwd:'$mongoPassword',roles:['userAdminAnyDatabase','dbAdminAnyDatabase','readWriteAnyDatabase']})"
-        # Enable MongoDB authorization
-        echo "security:" >> /etc/mongod.conf
-        echo "  authorization: \"enabled\"">> /etc/mongod.conf
-        service mongod restart
-
-    sleep 2
-
-        ## Add InfluxDB Admin user
-        influxUsername="user$(openssl rand -hex 16)"
-        influxPassword=$(openssl rand -hex 32)
-        echo "INFLUXDB_USERNAME = '$influxUsername'">> $BD/DataService/ds_config
-        echo "INFLUXDB_PWD = '$influxPassword'">> $BD/DataService/ds_config
-        sleep 1
-        curl -d "q=CREATE USER $influxUsername WITH PASSWORD '$influxPassword' WITH ALL PRIVILEGES" -X POST http://localhost:8086/query
-        sed -ir 's/# auth-enabled = false/auth-enabled = true/g' /etc/influxdb/influxdb.conf
-        service influxdb restart
-
-    sleep 2
-
-        ## Add Redis Admin user
-        redisPassword=$(openssl rand -hex 64)
-        echo "REDIS_PWD = '$redisPassword'">> $BD/CentralService/cs_config
-        echo "REDIS_PWD = '$redisPassword'">> $BD/DataService/ds_config
-        echo "    REDIS_PWD = '$redisPassword'" >> $BD/CentralReplica/config.py
-        sed -i -e '/#.* requirepass / s/.*/ requirepass  '$redisPassword'/' /etc/redis/redis.conf
-        service redis restart
-
-    sleep 2
-
-        ## Add RabbitMQ Admin user
-        rabbitmqUsername="user$(openssl rand -hex 16)"
-        rabbitmqPassword=$(openssl rand -hex 32)
-        rabbitmqUsername_endUser="user$(openssl rand -hex 16)"
-        rabbitmqPassword_endUser=$(openssl rand -hex 32)
-        echo "RABBITMQ_ADMIN_USERNAME = '$rabbitmqUsername'">> $BD/DataService/ds_config
-        echo "RABBITMQ_ADMIN_PWD = '$rabbitmqPassword'">> $BD/DataService/ds_config
-        echo "RABBITMQ_ENDUSER_USERNAME = '$rabbitmqUsername_endUser'">> $BD/DataService/ds_config
-        echo "RABBITMQ_ENDUSER_PWD = '$rabbitmqPassword_endUser'">> $BD/DataService/ds_config
-        # Create a Admin user.
-        rabbitmqctl add_user "$rabbitmqUsername" "$rabbitmqPassword"
-        # Add Administrative Rights
-        rabbitmqctl set_user_tags "$rabbitmqUsername" administrator
-        # Grant necessary permissions
-        rabbitmqctl set_permissions -p / "$rabbitmqUsername" ".*" ".*" ".*"
-        # Create a End User.
-        rabbitmqctl add_user "$rabbitmqUsername_endUser" "$rabbitmqPassword_endUser"
-        # Add Permissions
-        rabbitmqctl set_user_tags "$rabbitmqUsername_endUser"
-        # Grant necessary permissions
-        rabbitmqctl set_permissions -p / "$rabbitmqUsername_endUser" "" "" ".*"
-
-        echo "BuildingDepot uses RabbitMQ Queues for Publishing and Subscribing to Sensor data. "
-        echo "Some web front-end use RabbitMQ Queues use rabbitmq_web_stomp plugin"
-        echo "Enter Y to install rabbitmq_web_stomp plugin: "
-        read response
-        if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
-          rabbitmq-plugins enable rabbitmq_web_stomp
-        fi
+      rabbitmq-plugins enable rabbitmq_web_stomp
+    fi
 
     sleep 1
 
-        echo
-        echo "Auto-Generated User Credentials for BuildingDepot Packages [MongoDB,InfluxDB & Redis]"
-        echo
+    echo
+    echo "Auto-Generated User Credentials for BuildingDepot Packages [MongoDB,InfluxDB & Redis]"
+    echo
 
   elif [ "$response" == 'N' ] || [ "$response" == 'n' ]; then
     ## Add MongoDB Admin user
@@ -381,7 +381,7 @@ function setup_packages() {
 
     sleep 2
 
-   ## Add RabbitMQ Admin user
+    ## Add RabbitMQ Admin user
     echo
     echo "Enter RabbitMQ Admin Username: "
     read rabbitmqUsername
@@ -405,10 +405,10 @@ function setup_packages() {
     # Grant necessary permissions
     rabbitmqctl set_permissions -p / "$rabbitmqUsername_endUser" "" "" ".*"
 
-    echo "RABBITMQ_ADMIN_USERNAME = '$rabbitmqUsername'">> $BD/DataService/ds_config
-    echo "RABBITMQ_ADMIN_PWD = '$rabbitmqPassword'">> $BD/DataService/ds_config
-    echo "RABBITMQ_ENDUSER_USERNAME = '$rabbitmqUsername_endUser'">> $BD/DataService/ds_config
-    echo "RABBITMQ_ENDUSER_PWD = '$rabbitmqPassword_endUser'">> $BD/DataService/ds_config
+    echo "RABBITMQ_ADMIN_USERNAME = '$rabbitmqUsername'" >>$BD/DataService/ds_config
+    echo "RABBITMQ_ADMIN_PWD = '$rabbitmqPassword'" >>$BD/DataService/ds_config
+    echo "RABBITMQ_ENDUSER_USERNAME = '$rabbitmqUsername_endUser'" >>$BD/DataService/ds_config
+    echo "RABBITMQ_ENDUSER_PWD = '$rabbitmqPassword_endUser'" >>$BD/DataService/ds_config
 
     echo "BuildingDepot uses RabbitMQ Queues for Publishing and Subscribing to Sensor data. "
     echo "Some web front-end use RabbitMQ Queues use rabbitmq_web_stomp plugin"
@@ -425,9 +425,9 @@ function setup_packages() {
     echo
 
   else
-      echo
-      echo "Invalid option please Try again."
-      setup_packages
+    echo
+    echo "Invalid option please Try again."
+    setup_packages
   fi
 
 }
