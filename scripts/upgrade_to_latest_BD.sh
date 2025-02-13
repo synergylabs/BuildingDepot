890e6cfa56eafae0475f8a95d41484e7da2c726e
Ben Weinshel <bweinshel@cmu.edu>
2025-02-13 11:17:46 -0500

    do not delete or copy config files in upgrade script

--- scripts/upgrade_to_latest_BD.sh
+++ scripts/upgrade_to_latest_BD.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -euxo pipefail
 
-echo "Please make sure you have downloaded the latest Building Depot version and are in the BD scripts folder!"
+echo "Please make sure you have downloaded the latest Building Depot version and are in the BD scripts folder! Press enter to confirm."
 read response
 cd ../
 cd buildingdepot
-rm CentralReplica/config.py CentralService/cs_config DataService/ds_config
-sudo cp -rf . /srv/buildingdepot/
+
+# copy code files, excluding config files which are expected to already be in /srv/buildingdpot
+sudo rsync -a . /srv/buildingdepot/ --exclude CentralReplica/config.py --exclude CentralService/cs_config --exclude DataService/ds_config
+
+# restart services
 sudo supervisorctl restart all
