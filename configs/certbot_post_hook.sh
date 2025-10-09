7ff8cfc18ab70590c1aadfb32146765a642a5374
Synergy Labs <sysadmin@synergylabs.org>
2025-10-09 17:56:41 +0000

    updating install script

--- configs/certbot_post_hook.sh
+++ configs/certbot_post_hook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir /etc/rabbitmq/ssl/
-cp -rL --remove-destination <cert_path> /etc/rabbitmq/ssl/fullchain.pem
-cp -rL --remove-destination <server_cert_path> /etc/rabbitmq/ssl/cert.pem
-cp -rL --remove-destination <privkey_path> /etc/rabbitmq/ssl/privkey.pem
+cp -rL --remove-destination /etc/letsencrypt/live/dev-buildingdepot-synergy.mites.io/fullchain.pem /etc/rabbitmq/ssl/fullchain.pem
+cp -rL --remove-destination /etc/letsencrypt/live/dev-buildingdepot-synergy.mites.io/cert.pem /etc/rabbitmq/ssl/cert.pem
+cp -rL --remove-destination /etc/letsencrypt/live/dev-buildingdepot-synergy.mites.io/privkey.pem /etc/rabbitmq/ssl/privkey.pem
 chmod -R 555 /etc/rabbitmq/ssl/
 service rabbitmq-server restart
